bc5fa12103e360343916272a349b15b4553e8982
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-04-18 21:22:07 +0530

    unprotected routes (#6)

--- src/api/product/index.js
+++ src/api/product/index.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const factory = require("../_util/handlerFactory");
 const Product = require("../../models/Product");
+const productController = require("./controller/productController");
 
 router.get("/", factory.getAll(Product, "products"));
 
 router.get("/:id", factory.getOne(Product, "product"));
 
+router.get("/category/:categoryId", productController.getProductsByCategory);
+
+// TO be removed
 router.post("/", factory.createOne(Product, "product"));
 
 module.exports = router;
