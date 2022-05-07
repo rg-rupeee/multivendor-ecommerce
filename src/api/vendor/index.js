7f6e1496d3ba38d9d2ff86f18c2178ebc433913a
Sheikh Saifuddin Ahmed <76834891+saifuddin1703@users.noreply.github.com>
2022-05-07 21:19:43 +0530

    Vendor routes (#22)
    * vendor methods added
    
    * updated vendor

--- src/api/vendor/index.js
+++ src/api/vendor/index.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const OrgUser = require("../../models/OrgUser");
+const { protect } = require("../_util/middlewares/authMiddlewares");
 const router = express.Router();
+const factory = require("../_util/handlerFactory");
 
 const authRouter = require("./auth/index");
+const Vendor = require("../../models/Vendor");
 router.use("/auth", authRouter);
+
+// GET - get all <A>
+router.get(
+    "/",
+    protect(OrgUser),
+    factory.getAll(Vendor,"vendor")
+);
+
+// GET - get by id < A>
+router.get(
+    "/:id",
+    protect(OrgUser),
+    factory.getOne(Vendor,"vendor")
+);
+
+// DELETE - delete < A>
+router.delete(
+    "/:id",
+    protect(OrgUser),
+    factory.deleteOne(Vendor,"vendor")
+);
 
 module.exports = router;
