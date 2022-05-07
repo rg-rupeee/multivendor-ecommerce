c642e6ecd441c2d9f0a09f046ada213328cbfd65
Sheikh Saifuddin Ahmed <76834891+saifuddin1703@users.noreply.github.com>
2022-05-07 21:20:05 +0530

    blog routes (#21)

--- src/api/blog/index.js
+++ src/api/blog/index.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const router = express.Router();
-
+const {protect} = require("../_util/middlewares/authMiddlewares")
 const factory = require("../_util/handlerFactory");
 const Blog = require("../../models/Blog");
-
-router.get("/", factory.getAll(Blog, "blogs"));
+const OrgUser = require("../../models/OrgUser")
 
 router.get("/:id", factory.getOne(Blog, "blog"));
 
-// TO be removed
-router.post("/", factory.createOne(Blog, "blog"));
+// GET - get all <A>
+router.get(
+    "/", 
+    protect(OrgUser),
+    factory.getAll(Blog, "blogs")
+);
+
+// GET - get by id < A>
+router.get(
+    "/:id", 
+    protect(OrgUser),
+    factory.getOne(Blog, "blogs")
+);
+
+// POST - create <A>
+router.post(
+    "/", 
+    protect(OrgUser),
+    factory.createOne(Blog, "blogs")
+);
+
+// DELETE - delete < A>
+router.delete(
+    "/:id", 
+    protect(OrgUser),
+    factory.deleteOne(Blog, "blogs")
+);
+
+// PATCH - update <A>
+router.patch(
+    "/:id", 
+    protect(OrgUser),
+    factory.updateOne(Blog, "blogs")
+);
 
 module.exports = router;
