1418a3ae5ef8e36aca67dc94e8b11f8782acd3d7
saifuddin1703 <saifuddinahmed50@gmail.com>
2022-05-25 22:20:34 +0530

    order changes

--- src/api/order/index.js
+++ src/api/order/index.js
@@ -1,4 +1,34 @@
 const express = require("express");
+const Vendor = require("../../models/Vendor");
+const { protect } = require("../_util/middlewares/authMiddlewares");
 const router = express.Router();
+const factory = require("../_util/handlerFactory")
+const Order = require("../../models/Order");
+const OrgUser = require("../../models/OrgUser");
+
+// GET - get all <V,>
+router.get(
+    "/",
+    protect(Vendor),
+    factory.getAll(Order,"Order")
+);
+
+// GET - get vendors orders <V>
+
+
+// GET - get by id <V, A> (check)
+router.get(
+    "/:id",
+    protect(Vendor,OrgUser),
+    factory.getOne(Order,"Order")
+);
+
+// POST - create <V>
+router.post(
+    "/",
+    protect(Vendor),
+    factory.createOne(Order,"Order")
+);
+// PATCH - update <V, A,> (check)
 
 module.exports = router;
