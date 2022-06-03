cab3ccd94648e6312ead77c009327fe8708b1098
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-06-04 01:13:05 +0530

    changes

--- src/api/coupon/index.js
+++ src/api/coupon/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const factory = require("../_util/handlerFactory");
 const { protect } = require("../_util/middlewares/authMiddlewares");
+const { requiredFields } = require("../_util/check");
 const OrgUser = require("../../models/OrgUser");
 const Coupon = require("../../models/Coupon");
 
@@ -12,7 +13,12 @@ router.get("/", protect(OrgUser), factory.getAll(Coupon, "coupon"));
 router.get("/:id", protect(OrgUser), factory.getOne(Coupon, "coupon"));
 
 // POST - create <A>
-router.post("/", protect(OrgUser), factory.createOne(Coupon, "coupon"));
+router.post(
+  "/",
+  protect(OrgUser),
+  requiredFields("code", "description", "minPurchase", "maxDiscount"),
+  factory.createOne(Coupon, "coupon")
+);
 
 // DELETE - delete < A>
 router.delete("/:id", protect(OrgUser), factory.deleteOne(Coupon, "coupon"));
