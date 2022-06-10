55400c551e655620523314d512fa9569e7c48b33
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-06-10 15:58:34 +0530

    vendor module changes

--- src/api/vendor/index.js
+++ src/api/vendor/index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const OrgUser = require("../../models/OrgUser");
 const { protect } = require("../_util/middlewares/authMiddlewares");
+const { restrictedFields } = require("../_util/check");
 const router = express.Router();
 const factory = require("../_util/handlerFactory");
 
 const authRouter = require("./auth/index");
-const profileRouter = require("./profile/index")
+const meRoute = require("./me/index");
 const Vendor = require("../../models/Vendor");
 
 router.use("/auth", authRouter);
 
-//profile routes
-router.use("/profile",profileRouter);
-
-const meRoute = require("./me")
-router.use("/me",meRoute);
+router.use("/me", meRoute);
 
 // GET - get all <A>
 router.get("/", protect(OrgUser), factory.getAll(Vendor, "vendor"));
@@ -22,7 +19,15 @@ router.get("/", protect(OrgUser), factory.getAll(Vendor, "vendor"));
 // GET - get by id < A>
 router.get("/:id", protect(OrgUser), factory.getOne(Vendor, "vendor"));
 
+router.patch(
+  "/:id",
+  protect(OrgUser),
+  restrictedFields("password"),
+  factory.updateOne(Vendor, "vendor")
+);
+
 // DELETE - delete < A>
+// TODO: all products should mark vendor as not verified and all of its products as unpublished
 router.delete("/:id", protect(OrgUser), factory.deleteOne(Vendor, "vendor"));
 
 module.exports = router;
--- src/api/vendor/me/index.js
+++ src/api/vendor/me/index.js
@@ -1,24 +1,19 @@
-const express = require('express');
-const OrgUser = require('../../../models/OrgUser');
-const Vendor = require('../../../models/Vendor');
-const { restrictedFields } = require('../../_util/check');
-const { protect } = require('../../_util/middlewares/authMiddlewares');
+const express = require("express");
+const OrgUser = require("../../../models/OrgUser");
+const Vendor = require("../../../models/Vendor");
+const { restrictedFields } = require("../../_util/check");
+const { protect } = require("../../_util/middlewares/authMiddlewares");
 const router = express.Router();
 const factory = require("../../_util/handlerFactory");
-const profileController = require('../profile/controller/profileController');
+const profileController = require("./controller/profileController");
 
 router.patch(
-    "/profile",
-    restrictedFields("isVerified"),
-    protect(Vendor),
-    profileController.updateProfile
-)
+  "/profile",
+  restrictedFields("isVerified", "password"),
+  protect(Vendor),
+  profileController.updateProfile
+);
 
-router.get(
-    "/profile",
-    protect(Vendor),
-    profileController.getMyProfile
-)
-
+router.get("/profile", protect(Vendor), profileController.getMyProfile);
 
 module.exports = router;
