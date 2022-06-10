3b43ee291aa31fc6f86b7b2939f33e876448956e
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-06-10 11:12:16 +0530

    controller

--- src/api/admin/me/index.js
+++ src/api/admin/me/index.js
@@ -3,20 +3,18 @@ const OrgUser = require("../../../models/OrgUser");
 const { protect } = require("../../_util/middlewares/authMiddlewares");
 const router = express.Router();
 
-const profileController = require("./profile/controller/profileController");
-
-const factory = require("../../_util/handlerFactory")
+const profileController = require("../profile/controller/profileController");
 
 router.get(
-    "/profile",
-    protect(OrgUser,"orgUser"),
-    profileController.getMyProfile
+  "/profile",
+  protect(OrgUser, "orgUser"),
+  profileController.getMyProfile
 );
 
 router.patch(
-    "/profile",
-    protect(OrgUser,"orgUser"),
-    profileController.updateProfile
+  "/profile",
+  protect(OrgUser, "orgUser"),
+  profileController.updateProfile
 );
 
 module.exports = router;
