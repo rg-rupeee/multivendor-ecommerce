a544368018eea2710519c1ebcd27e91476288297
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2022-06-16 19:39:25 +0530

    get all users

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const { protect } = require("../_util/middlewares/authMiddlewares");
+const factory = require("../_util/handlerFactory");
+const OrgUser = require("../../models/OrgUser");
+const User = require("../../models/User");
+
+router.get("/all", protect(OrgUser), factory.getAll(User));
+
 const meRouter = require("./me/index");
 router.use("/me", meRouter);
 
