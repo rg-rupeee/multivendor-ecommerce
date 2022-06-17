4f83731d98245c2c5ee5f3cc9d928bf8ff5ba5d9
saifuddin1703 <saifuddinahmed50@gmail.com>
2022-06-17 22:14:02 +0530

    resolved conflicts

--- src/api/statistics/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const productStatsRouter = require("./product/index")
-router.use("/products",productStatsRouter);
-module.exports = router;
--- src/api/statistics/product/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require("./controller/productsStatsController")
-
-router.get(
-    "/",
-    controller.productWeeklyStats
-)
-module.exports = router;
