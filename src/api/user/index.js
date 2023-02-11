fe0ab395d9c1362b380fa1afe59c67a7e2fbb0ca
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2023-02-11 20:34:43 +0530

    api to serch user

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -5,10 +5,17 @@ const { protect } = require("../_util/middlewares/authMiddlewares");
 const factory = require("../_util/handlerFactory");
 const OrgUser = require("../../models/OrgUser");
 const User = require("../../models/User");
+const { requiredFields } = require("../_util/check");
 
 router.get("/all", protect(OrgUser), factory.getAll(User));
 
 router.get("/all/:id", protect(OrgUser), factory.getOne(User));
+
+router.post(
+  "/search",
+  requiredFields("searchKey"),
+  factory.search(User, "email")
+);
 
 const meRouter = require("./me/index");
 router.use("/me", meRouter);
